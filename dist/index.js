01236de7dbe6cc0f3576951524a421c4fbf9999a
Mayoorann <navamayoo@gmail.com>
2023-10-13 10:36:04 +0530

    init Commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = index_component;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function index_component() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "This is a NPM REACT COMPONENT"));
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index_component() {
+  return (
+    <div>
+        <h1>This is a NPM REACT COMPONENT</h1>
+    </div>
+  )
+}
