5628b1762cc0b1b8030481d810ef70e09bf5aad6
Mayoorann <navamayoo@gmail.com>
2023-10-13 11:19:01 +0530

    Add Mui Button

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,28 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = index_component;
 var _react = _interopRequireDefault(require("react"));
+var _Button = _interopRequireDefault(require("@mui/material/Button"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function index_component() {
-  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "This is a NPM REACT COMPONENT"));
+  var handleSecondary = function handleSecondary() {
+    alert("You are click Secondary Button");
+  };
+  var handleSuccess = function handleSuccess() {
+    alert("You are click Success Button");
+  };
+  var handleError = function handleError() {
+    alert("You are click Error Button");
+  };
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "This is a NPM REACT COMPONENT"), /*#__PURE__*/_react["default"].createElement(_Button["default"], {
+    color: "secondary",
+    onClick: handleSecondary
+  }, "Secondary"), /*#__PURE__*/_react["default"].createElement(_Button["default"], {
+    variant: "contained",
+    color: "success",
+    onClick: handleSuccess
+  }, "Success"), /*#__PURE__*/_react["default"].createElement(_Button["default"], {
+    variant: "outlined",
+    color: "error",
+    onClick: handleError
+  }, "Error"));
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import Button from "@mui/material/Button";
 
 export default function index_component() {
+
+ const handleSecondary=()=>{
+alert("You are click Secondary Button");
+  }
+   const handleSuccess = () => {
+    alert("You are click Success Button");
+   };
+      const handleError = () => {
+        alert("You are click Error Button");
+      };
+
   return (
     <div>
-        <h1>This is a NPM REACT COMPONENT</h1>
+      <h1>This is a NPM REACT COMPONENT</h1>
+      <Button color="secondary" onClick={handleSecondary}>
+        Secondary
+      </Button>
+      <Button variant="contained" color="success" onClick={handleSuccess}>
+        Success
+      </Button>
+      <Button variant="outlined" color="error" onClick={handleError}>
+        Error
+      </Button>
     </div>
-  )
+  );
 }
